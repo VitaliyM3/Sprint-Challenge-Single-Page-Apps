d5a24df4a60af5396ce1b865765e3370bda2fd95
Vitaliy Mikhaylyyk <vitaliymikhaylyuk@gmail.com>
2019-12-15 22:58:54 -0500

    char card, char list done and works

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Card = styled.div`
+display: flex;
+width: 300px;
+height: 400px;
+flex-wrap: wrap;
+border: 2px solid blue;
+justify-content: space-between;
+margin: 10px auto;
+padding: 10px;
+
+`
+
+
+export default function CharacterCard(props) {
+  return (
+    <Card>
+      <h2> {props.name} </h2>
+      <img src={props.image} />
+      <p> {props.species} </p>
+    </Card>
+  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+import CharacterList from "./CharacterList";
+import WelcomePage from "./WelcomePage";
+
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Route exaxt path="/" render={props => <WelcomePage {...props} />} />
+      <Route path="/characterlist" render={props => <CharacterList {...props} />} />
+      <Link to="/">Home</Link>
+      <Link to="/characterlist">Characters</Link>
     </header>
   );
 }
